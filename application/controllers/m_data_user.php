7d34ab5d52ceb98125862a930742af0e26674d9b
Rola Setia Putra <rolasetia24@gmail.com>
2019-07-22 15:47:01 +0700

    menu master

--- application/controllers/m_data_user.php
+++ application/controllers/m_data_user.php
@@ -3,12 +3,12 @@
 class M_data_user extends CI_Controller {
 	function __construct() {
 		parent::__construct();
-		$this->load->model(array('web_model','departemen_model'));
+		$this->load->model(array('web_model','m_user_model'));
 	}
 
 	public function master_data_user (){
-		$data['page']		= "l_request_d2p";	
-		$data['departemen'] = $this->departemen_model->getAllDepDivisi();
+		$data['page']		= "l_master_user";	
+		$data['datauser'] = $this->m_user_model->getAlluser();
 		$this->load->view('admin/aaa', $data);
 	}
 
--- application/models/m_user_model.php
+++ application/models/m_user_model.php
@@ -6,11 +6,15 @@ class M_user_model extends CI_Model {
 	public function getAlluser(){
     	$this->db->select('*');
     	$this->db->from('tb_user');
+    	$this->db->join('tb_role_access', 'tb_user.id_role_access = tb_role_access.id_role_access');
+
     $query = $this->db->get();
         if ($query->num_rows() > 0){
             return $query->result();
         } else {
             return array();
         }    
+
+    }
 
 }
